e76378c3ca1bb8de260dc3d1e054a3055efd0a1f
Dunatron <heath.dunlop.hd@gmail.com>
2022-04-27 22:07:49 +1200

    ff

--- used-car-pricing/src/users/users.controller.ts
+++ used-car-pricing/src/users/users.controller.ts
@@ -8,6 +8,8 @@ import {
   Query,
   Delete,
   NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -23,6 +25,7 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
